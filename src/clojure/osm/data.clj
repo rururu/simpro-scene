d887b7549ff73c2f1fc520f80aa39875bcdf551f
Ruslan Sorokin <ru@Ruslans-iMac.local>
2018-01-14 23:44:11 +0300

    Railway creation

--- src/clojure/osm/data.clj
+++ src/clojure/osm/data.clj
@@ -1,13 +1,19 @@
 (ns osm.data
+(:use protege.core) 
 (:require
-   [clojure.data.json :as json]
-   [clojure.xml :as x])
+   [clojure.data.json :as json])
 (:import
   ru.igis.omtab.MapOb
   ru.igis.omtab.OMT
-  ru.igis.omtab.OMTPoly))
+  ru.igis.omtab.OMTPoly
+  ru.igis.omtab.gui.RuMapMouseAdapter))
 
 (def OSM-DATA (volatile! []))
+(def RMMA false)
+(def MODE nil)
+(def RADIUS 0)
+(def RR-COLOR "FF0000FF")
+(def RW-COLOR "FFFF0000")
 (defn railway-api-url [bbx]
   (let [[w s e n] bbx]
   (str "http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:json];(way[railway](" s "," w "," n "," e "););out%20body;%3E;out%20skel%20qt;")))
@@ -15,7 +21,7 @@
 (defn railway-data [bbx]
   (try
   (let [url (railway-api-url bbx)
-         _ (println :URL url)
+         ;;_ (println :URL url)
          jsn (json/read-str (slurp url) :key-fn keyword)]
       jsn)
   (catch Exception e
@@ -69,19 +75,71 @@
       (filter-kkv [:tags :railway] "rail")
       (id-way-points idp)))))
 
-(defn op-rail-lines [rails color]
-  (letfn [(omp [id r]
-              (let [[[la1 lo1] [la2 lo2]] [(first r) (last r)]
-	[lat1 lon1] [(MapOb/getDegMin la1) (MapOb/getDegMin lo1)]
-	[lat2 lon2] [(MapOb/getDegMin la2) (MapOb/getDegMin lo2)]]
-                (OMTPoly/createOMTPoly 
-	(str id)
-	lat1
-	lon1
-	color
-	nil
-	"false"
-	"false"
-	[(str lat1 " " lon1) (str lat2 " " lon2)])))]
-  (reduce-kv #(assoc %1 %2 [(omp %2 %3) %3]) {} rails)))
+(defn create-railway [[id rail]]
+  (if (fifos "Railway" "id" (str id))
+  []
+  (let [[[la1 lo1] [la2 lo2]] [(first rail) (last rail)]
+         [lat1 lon1] [(MapOb/getDegMin la1) (MapOb/getDegMin lo1)]
+         [lat2 lon2] [(MapOb/getDegMin la2) (MapOb/getDegMin lo2)]
+         rwi (crin "Railway")
+         poi (crin "OMTPoly")]
+    (ssv poi "label" (str id))
+    (ssv poi "latitude" lat1)
+    (ssv poi "longitude" lon1)
+    (ssv poi "lineColor" RW-COLOR)
+    (ssvs poi "points" [(str lat1 " " lon1) (str lat2 " " lon2)])
+    (ssv rwi "id" (str id))
+    (ssv rwi "poly" poi)
+    (ssv rwi "source" (str (vec rail)))
+    [poi])))
+
+(defn add-railway [llp]
+  (println :MODE MODE)
+(get-railway-data (seq llp) RADIUS)
+(if (nil? @OSM-DATA)
+  (println :NO-DATA)
+  (let [opr (op-rails @OSM-DATA)
+         rws (mapcat create-railway opr)]
+    (println "Created" (count rws) "railways")
+    (doseq [rw rws]
+      (OMT/addMapOb rw)))))
+
+(defn remove-railway [mo]
+  (println :MODE MODE))
+
+(defn set-mouse-adapter []
+  (let [rmma (proxy [RuMapMouseAdapter] []
+	(mouseLeftButtonAction [mo llp runa]
+                          ;; (println MODE mo llp runa)
+	  (condp = MODE
+	    'ADD (add-railway llp)
+	    'REMOVE (remove-railway mo)
+	    (println (or (if mo (.getName mo)) (seq llp))))
+	  true))
+       pgs (seq (OMT/getPlaygrounds))]
+  (.setRuMapMouseAdapter (first pgs) rmma)
+  (def RMMA true)))
+
+(defn mode-add [hm inst]
+  (if (not RMMA)
+  (set-mouse-adapter))
+(def MODE 'ADD)
+(def RADIUS (sv inst "radius"))
+(ssv inst "status" "MODE ADD"))
+
+(defn mode-remove [hm inst]
+  (if (= MODE 'ADD)
+  (do (def MODE 'REMOVE)
+    (ssv inst "status" "MODE REMOVE"))
+  (ssv inst "status" "Add railways before")))
+
+(defn create-railroad [hm inst]
+  (let [mp (into {} hm)
+       frm (mp "from1")
+       to (mp "to1")]
+(if (or (= MODE 'ADD) (= MODE 'REMOVE))
+  (do (def MODE 'CREATE)
+    (ssv inst "status" "MODE CREATE RAILROAD")
+    (println :FROM frm :TO to))
+  (ssv inst "status" "Add railways before"))))
 
