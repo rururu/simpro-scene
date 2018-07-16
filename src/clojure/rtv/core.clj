dfef3ef8fc649148990e41783b1d6f785804a02a
Ruslan Sorokin <ru@Ruslans-iMac.local>
2018-07-17 00:32:39 +0300

    RtV Program

---
+++ src/clojure/rtv/core.clj
@@ -0,0 +1,24 @@
+(ns rtv.core
+(:use protege.core
+  subcla.etoele))
+
+(def D5MIN (/ 1 60))
+(defn gradient [lat lon]
+  (let [ec    (elevation [lat lon])
+       en    (elevation [(+ lat D5MIN) lon])
+       ene  (elevation [(+ lat D5MIN) (+ lon D5MIN)])
+       enw (elevation [(+ lat D5MIN) (- lon D5MIN)])
+       es    (elevation [(- lat D5MIN) lon])
+       ese  (elevation [(- lat D5MIN) (+ lon D5MIN)])
+       esw (elevation [(- lat D5MIN) (- lon D5MIN)])
+       ee    (elevation [lat (+ lon D5MIN)])
+       ew   (elevation [lat (- lon D5MIN)])
+       mim (min ec en ene enw es ese esw ee ew)
+       maz (max ec en ene enw es ese esw ee ew)]
+  ;;(println ec en ene enw es ese esw ee ew)
+  (- maz mim)))
+
+(defn field [lat lon w h]
+  (doseq [y (range h)]
+  (println (for [x (range w)] (gradient (+ lat (* y 3 D5MIN)) (+ lon (* x 3 D5MIN)))))))
+
