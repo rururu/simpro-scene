ecd97f5f2ca49ba1d9240c7c1ec8c9cc1836b17a
Ruslan Sorokin <ru@Ruslans-iMac.local>
2018-07-17 22:36:39 +0300

    gradientPoly function for RasterToVector project

--- src/clojure/rtv/core.clj
+++ src/clojure/rtv/core.clj
@@ -1,8 +1,12 @@
 (ns rtv.core
 (:use protege.core
-  subcla.etoele))
+  subcla.etoele)
+(:import
+  ru.igis.omtab.OMTPoly
+  ru.igis.omtab.MapOb
+  ru.igis.omtab.OMT))
 
-(def D5MIN (/ 1 60))
+(def D5MIN (/ 1 12))
 (defn gradient [lat lon]
   (let [ec    (elevation [lat lon])
        en    (elevation [(+ lat D5MIN) lon])
@@ -21,4 +25,20 @@
 (defn field [lat lon w h]
   (doseq [y (range h)]
   (println (for [x (range w)] (gradient (+ lat (* y 3 D5MIN)) (+ lon (* x 3 D5MIN)))))))
+
+(defn gradientPoly [lat lon]
+  (let [gra (gradient lat lon)
+       gpo (OMTPoly/createOMTPoly
+	"po1"
+	(MapOb/getDegMin lat)
+	(MapOb/getDegMin lon)
+	"FFFF0000"
+	"20FF0000"
+	"true"
+	"false"
+	[(str (MapOb/getDegMin lat) " " (MapOb/getDegMin lon))
+	 (str (MapOb/getDegMin lat) " " (MapOb/getDegMin (+ lon (* 3 D5MIN))))
+	 (str (MapOb/getDegMin (- lat (* 3 D5MIN))) " " (MapOb/getDegMin (+ lon (* 3 D5MIN))))
+	 (str (MapOb/getDegMin (- lat (* 3 D5MIN))) " " (MapOb/getDegMin lon))])]
+  (OMT/addMapOb gpo (second (OMT/getPlaygrounds)))))
 
