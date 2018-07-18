97c468da1953e1f916bb9a6a36b784f7ff97bc09
rururu <zspovenetsky@gmail.com>
2018-07-18 21:50:28 +0300

    field of gradient objects

--- src/clojure/rtv/core.clj
+++ src/clojure/rtv/core.clj
@@ -22,23 +22,37 @@
   ;;(println ec en ene enw es ese esw ee ew)
   (- maz mim)))
 
-(defn field [lat lon w h]
-  (doseq [y (range h)]
-  (println (for [x (range w)] (gradient (+ lat (* y 3 D5MIN)) (+ lon (* x 3 D5MIN)))))))
+(defn grad-to-transpar [grad gmax color]
+  (let [itr (int (* (/ 255 gmax) grad))
+       itr (if (> itr 255) 255 itr)
+       xtr (format "%02x" itr)] 
+  (str xtr (.substring color 2))))
 
-(defn gradientPoly [lat lon]
+(defn gradientPoly [id lat lon gmax color]
   (let [gra (gradient lat lon)
+       ;;_ (println gra)
+       gtp (grad-to-transpar gra gmax color)
        gpo (OMTPoly/createOMTPoly
-	"po1"
+	(str id)
 	(MapOb/getDegMin lat)
 	(MapOb/getDegMin lon)
-	"FFFF0000"
-	"20FF0000"
+	gtp
+	gtp
 	"true"
 	"false"
 	[(str (MapOb/getDegMin lat) " " (MapOb/getDegMin lon))
-	 (str (MapOb/getDegMin lat) " " (MapOb/getDegMin (+ lon (* 3 D5MIN))))
-	 (str (MapOb/getDegMin (- lat (* 3 D5MIN))) " " (MapOb/getDegMin (+ lon (* 3 D5MIN))))
-	 (str (MapOb/getDegMin (- lat (* 3 D5MIN))) " " (MapOb/getDegMin lon))])]
-  (OMT/addMapOb gpo (second (OMT/getPlaygrounds)))))
+	 (str (MapOb/getDegMin lat) " " (MapOb/getDegMin (+ lon (* 1 D5MIN))))
+	 (str (MapOb/getDegMin (- lat (* 1 D5MIN))) " " (MapOb/getDegMin (+ lon (* 1 D5MIN))))
+	 (str (MapOb/getDegMin (- lat (* 1 D5MIN))) " " (MapOb/getDegMin lon))])
+       mo (OMT/addMapOb gpo (second (OMT/getPlaygrounds)))]
+  (.hideLabel mo)))
+
+(defn field [lat lon w h]
+  (doseq [y (range h)]
+  (doseq [x (range w)] 
+   (gradientPoly (str x y)
+	(+ lat (* y 1 D5MIN)) 
+	(+ lon (* x 1 D5MIN))
+	500
+                        "FFFF0000"))))
 
