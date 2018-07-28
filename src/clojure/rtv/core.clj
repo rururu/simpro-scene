85b1d1724e3c3849d864b30cb891adc163caed4a
rururu <zspovenetsky@gmail.com>
2018-07-28 22:54:44 +0300

    fieldSlope function

--- src/clojure/rtv/core.clj
+++ src/clojure/rtv/core.clj
@@ -6,7 +6,7 @@
   ru.igis.omtab.MapOb
   ru.igis.omtab.OMT))
 
-(def D5MIN (/ 1 12))
+(def D5MIN (double (/ 1 12)))
 (defn gradient [lat lon]
   (let [ec    (elevation [lat lon])
        en    (elevation [(+ lat D5MIN) lon])
@@ -57,22 +57,47 @@
                         "FFFF0000"))))
 
 (defn slope1 [lat lon]
-  (let [c    (elevation [lat lon])
-       n    (elevation [(+ lat D5MIN) lon])
-       ne  (elevation [(+ lat D5MIN) (+ lon D5MIN)])
-       nw (elevation [(+ lat D5MIN) (- lon D5MIN)])
-       s    (elevation [(- lat D5MIN) lon])
-       se  (elevation [(- lat D5MIN) (+ lon D5MIN)])
-       sw (elevation [(- lat D5MIN) (- lon D5MIN)])
-       e    (elevation [lat (+ lon D5MIN)])
-       w   (elevation [lat (- lon D5MIN)])
-       dif (fn [x] (- (apply max x) (apply min x)))
-       m {(dif [w c e])    :hor
-             (dif [s c n])     :ver
-             (dif [sw c ne]) :rig
-             (dif [se c nw]) :lef}]
+  (let [cde (fn [p] [p (elevation p)])
+       c    (cde [lat lon])
+       n    (cde [(+ lat D5MIN) lon])
+       ne  (cde [(+ lat D5MIN) (+ lon D5MIN)])
+       nw (cde [(+ lat D5MIN) (- lon D5MIN)])
+       s    (cde [(- lat D5MIN) lon])
+       se  (cde [(- lat D5MIN) (+ lon D5MIN)])
+       sw (cde [(- lat D5MIN) (- lon D5MIN)])
+       e    (cde [lat (+ lon D5MIN)])
+       w   (cde [lat (- lon D5MIN)])
+       dif (fn [x] (let [x (map second x)]
+	(- (apply max x) (apply min x))))
+       m (hash-map 
+	(dif [w c e])    [(first w) (first e)]
+	(dif [s c n])     [(first s) (first n)]
+	(dif [sw c ne]) [(first sw) (first ne)]
+	(dif [nw c se]) [(first nw) (first se)])]
   (m (apply min (keys m)))))
 
-(defn dif [lst]
-  (- (apply max lst) (apply min lst)))
+(defn slope1Line [id lat lon color]
+  (let [slo (slope1 lat lon)
+       pnt #(str (MapOb/getDegMin (first %)) " " (MapOb/getDegMin (second %)))
+       lin [(pnt (first slo)) (pnt (second slo))]
+       _ (println lin)
+       gpo (OMTPoly/createOMTPoly
+	(str id)
+	(MapOb/getDegMin lat)
+	(MapOb/getDegMin lon)
+	color
+	color
+	"false"
+	"false"
+	lin)
+       mo (OMT/addMapOb gpo (second (OMT/getPlaygrounds)))]
+  (.hideLabel mo)))
+
+(defn fieldSlope [lat lon w h]
+  (doseq [y (range h)]
+  (doseq [x (range w)] 
+   (slope1Line (str x y)
+	(+ lat (* y 3 D5MIN)) 
+	(+ lon (* x 3 D5MIN))
+                        "FFFF0000"))))
 
