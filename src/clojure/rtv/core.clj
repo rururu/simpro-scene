7ebee12ac0f137335550df748017304ca59ee958
Ruslan Sorokin <ru@Ruslans-iMac.local>
2018-08-01 23:36:06 +0300

    RTV Onto

--- src/clojure/rtv/core.clj
+++ src/clojure/rtv/core.clj
@@ -7,6 +7,28 @@
   ru.igis.omtab.OMT))
 
 (def D5MIN (double (/ 1 12)))
+(def MAXD 11000)
+(def MAXH 9000)
+(def LEVCOL20 {0	"FD0000FF"
+1	"E60000FF"
+2	"CF0000FF"
+3	"B80000FF"
+4	"A10000FF"
+5	"8A0000FF"
+6	"730000FF"
+7	"5C0000FF"
+8	"450000FF"
+9	"2E0000FF"
+10	"170000FF"
+11	"1CFF0000"
+12	"38FF0000"
+13	"54FF0000"
+14	"70FF0000"
+15	"8CFF0000"
+16	"A8FF0000"
+17	"C4FF0000"
+18	"E0FF0000"
+19	"FCFF0000"})
 (defn gradient [lat lon]
   (let [ec    (elevation [lat lon])
        en    (elevation [(+ lat D5MIN) lon])
@@ -100,4 +122,93 @@
 	(+ lat (* y 3 D5MIN)) 
 	(+ lon (* x 3 D5MIN))
                         "FFFF0000"))))
+
+(defn level1 [ele disc]
+  (let [pele (+ ele MAXD)
+       rele (/ pele (+ MAXD MAXH))
+       dele (* rele disc)]
+  (int dele)))
+
+(defn isoPoly [id lat lon]
+  (let [disc 20
+       ele (elevation [lat lon])
+       lev (level1 ele disc)
+       ;;_ (println :lev lev)
+       color (LEVCOL20 lev)
+       pol (OMTPoly/createOMTPoly
+	(str id)
+	(MapOb/getDegMin lat)
+	(MapOb/getDegMin lon)
+	color
+	color
+	"true"
+	"false"
+	[(str (MapOb/getDegMin lat) " " (MapOb/getDegMin lon))
+	 (str (MapOb/getDegMin lat) " " (MapOb/getDegMin (+ lon (* 3 D5MIN))))
+	 (str (MapOb/getDegMin (- lat (* 3 D5MIN))) " " (MapOb/getDegMin (+ lon (* 3 D5MIN))))
+	 (str (MapOb/getDegMin (- lat (* 3 D5MIN))) " " (MapOb/getDegMin lon))])
+           mo (OMT/addMapOb pol (second (OMT/getPlaygrounds)))]
+      (.hideLabel mo)))
+
+(defn fieldIso [lat lon w h]
+  (doseq [y (range h)]
+  (doseq [x (range w)] 
+   (isoPoly (str x y)
+	(+ lat (* y 3 D5MIN)) 
+	(+ lon (* x 3 D5MIN))))))
+
+(defn point
+  ([lab lat lon color]
+  (if-let [mo (OMT/getMapOb lab)]
+    (OMT/removeMapOb mo true))
+  (let [ins (crin "OMTPoint")]
+    (ssv ins "label" lab) 
+    (ssv ins "latitude" (MapOb/getDegMin lat))
+    (ssv ins "longitude" (MapOb/getDegMin lon))
+    (ssv ins "lineColor" color)
+    (ssv ins "oval" true)
+    (OMT/getOrAdd ins)))
+([lab lat lon]
+  (point lab lat lon "FFFF0000")))
+
+(defn levelPoint
+  ([id lat lon level disc color]
+  (let [ele (elevation [lat lon])
+         lev (level1 ele disc)]
+    (if (= lev level)
+      (let [mo (point (str id) lat lon color)]
+        (.setDescription mo (str "<html>id: " id 
+	"<br>lat: " lat 
+	"<br>lon: " lon
+	"<br>ele: " ele
+	"<br>lev: " lev))
+        (.hideLabel mo)))))
+([id lat lon]
+  (let [disc 20
+         ele (elevation [lat lon])
+         lev (level1 ele disc)
+         mo  (point (str id) lat lon (LEVCOL20 lev))]
+    (.setDescription mo (str "<html>id: " id 
+	"<br>lat: " lat 
+	"<br>lon: " lon
+	"<br>ele: " ele
+	"<br>lev: " lev))
+    (.hideLabel mo))))
+
+(defn pointsField
+  ([lat lon w h]
+  (doseq [y (range h)]
+    (doseq [x (range w)] 
+     (levelPoint (str x ":" y)
+	(+ lat (* y 3 D5MIN)) 
+	(+ lon (* x 3 D5MIN))))))
+([lat lon w h level disc color]
+  (doseq [y (range h)]
+    (doseq [x (range w)] 
+      (levelPoint (str x ":" y)
+	(+ lat (* y 3 D5MIN)) 
+	(+ lon (* x 3 D5MIN))
+	level
+	disc
+	color)))))
 
