9dec116c3a94645ac32f081ee168eb186d19c0ac
Ruslan Sorokin <ru@Ruslans-iMac.local>
2019-03-16 13:05:55 +0300

    Fixed SailExpert and OSMRoads projects

--- src/clojure/sim.clj
+++ src/clojure/sim.clj
@@ -13,12 +13,41 @@
 
 (def ES-TIMER nil)
 (def EVT-LISTENERS (volatile! {}))
+(defn mapob-event [evt]
+  ;;(println :MAPOB-EVENT (.getName (.getSource evt)))
+(let [obj (.getSource evt)
+       nob (instance? NavOb obj)]
+  (rete/assert-frame
+    ['MapObEvent 'status (.getActionCommand evt)
+	'label (.getName obj)
+	'lat (.getLatitude obj)
+	'lon (.getLongitude obj)
+	'course (if nob (.getCourse obj))
+	'speed (if nob (.getSpeed obj))
+	'altitude (if nob (.getAltitude obj))
+	'verticalSpeed (if nob (.getverticalSpeed obj))
+	'route (if nob (.getRoute obj)) ])))
+
+(defn ass-mo-event [evt]
+  ;;(println :ASS-MO-EVENT (.getActionCommand evt) (.getName (.getSource evt)))
+(let [sts (.getActionCommand evt)
+       obj (.getSource evt)]
+  (condp = sts 
+    "STOP_ROUTE"
+    (do (rete/assert-frame ['MapObEvent 
+	'status sts 
+	'object obj 
+	'lat (.getLatitude obj) 
+	'lon (.getLongitude obj)])
+      (rete/fire))
+    true)))
+
 (defn start-evt-listen []
   (doseq[pg (OMT/getPlaygrounds)]
   (let [al (proxy [ActionListener] []
 	(actionPerformed [evt] 
 	  ;;(println :OMT-GEN-EVT evt)
-  	(rete/assert-frame ['MapObEvent 'status (.getActionCommand evt) 'object (.getSource evt)])))]
+	  (ass-mo-event evt)))]
     (.addActionListener pg al)
     (vswap! EVT-LISTENERS assoc pg al))))
 
@@ -57,6 +86,29 @@
   (stop-sim)
 (start-sim))
 
-(defn reset-time []
-  (Clock/setClock 0))
+(defn exec-activity []
+  (letfn [(get-old [cln sln slv clz]
+	(if-let [ins (p/fifos cln sln slv)]
+	  (let [cly (.getDirectType ins)]
+	    (.addDirectType ins clz)
+	    (.removeDirectType ins cly)
+	    ins)))]
+  (let [clz (edu.stanford.smi.protege.ui.DisplayUtilities/pickCls nil p/*kb* [(p/cls "Activity")])
+         cln (.getName clz)
+         ins (or (get-old "Activity" "title" "test" clz) (p/foc cln "title" "test"))]
+    (when (clojuretab.ClojureTab/showModalInstance ins (str cln ". Fill Slots as Needed"))
+      (p/ssv ins "status" "START")
+      (-> (ru.rules/mk-frame ins)
+        (ru.rules/update-frame
+	:same-type
+	{'status "START"
+	 'run (HashMap.)})
+        rete.core/assert-frame)))))
+
+(defn toggle-visible []
+  (let [ii (OMT/getMapObInstances)
+       tt (DisplayUtilities/pickInstancesFromCollection nil ii "Toggle Labels Visible")]
+  (doseq [t tt]
+    (if-let[mo (OMT/getMapOb t)]
+      (.setVisible mo (not (.isVisible mo)) )) )))
 
