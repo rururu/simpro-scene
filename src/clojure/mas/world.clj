43895c66ca25959a9984c2d2cdf4b4e36d53c299
rururu <zspovenetsky@gmail.com>
2019-08-04 21:37:18 +0300

    MASCampus view map

--- src/clojure/mas/world.clj
+++ src/clojure/mas/world.clj
@@ -1,30 +1,34 @@
 (ns mas.world
 (:import
   java.net.URL
+  java.awt.Color
   sim.util.Bag
   sim.field.geo.GeomVectorField
   sim.io.geo.ShapeFileImporter
+  sim.util.geo.GeomPlanarGraph
   sim.util.geo.MasonGeometry
   sim.display.Display2D
+  sim.portrayal.geo.GeomPortrayal
+  sim.portrayal.geo.GeomVectorFieldPortrayal
   com.vividsolutions.jts.geom.Envelope
-  java.awt.Color))
-(def WIDTH 1000)
-(def HEIGHT 1000)
-(def NUM_AGENTS 50)
-(def buildings (GeomVectorField. WIDTH HEIGHT))
-(def walkways (GeomVectorField. WIDTH HEIGHT))
-(def roads (GeomVectorField. WIDTH HEIGHT))
-(def display nil)
-(def colorMap nil)
+  com.vividsolutions.jts.geom.GeometryFactory))
 (deftype Agent [astat ]
 	sim.engine.Steppable
 	(step [this agents] nil)
 )
-(deftype IWorldImpl []
+(deftype IWorldImpl [^java.util.HashMap wstate ]
 	ru.igis.sim.IWorld
-	(init [this] (let [buildings (GeomVectorField. 1000 1000)
-       roads (GeomVectorField. 1000 1000) 
-       walkways (GeomVectorField. 1000 1000)
+	(initialise [this] (let [width 1000
+       height 1000
+       num-agents 50
+       buildings (GeomVectorField. width height)
+       roads (GeomVectorField. width height) 
+       walkways (GeomVectorField. width height)
+       agents (GeomVectorField. width height) 
+       network (GeomPlanarGraph.)
+       netiter (.nodeIterator network)
+       fact (GeometryFactory.)
+       junctions (GeomVectorField. width height) 
        masked (Bag.)
        _ (.add masked "NAME")
        _ (.add masked "FLOORS")
@@ -41,11 +45,52 @@
   (println "Done reading data")
   (.setMBR buildings MBR)
   (.setMBR roads MBR)
-  (.setMBR walkways MBR)))
-	(start [this word] nil)
+  (.setMBR walkways MBR)
+  (.createFromGeomField network walkways)
+  (while (.hasNext netiter)
+    (let [node (.next netiter)
+           coord (.getCoordinate node)
+           point (.createPoint fact coord)]
+      (.addGeometry junctions (MasonGeometry. point))))
+  ;; Store world state
+  (.put wstate :width width)
+  (.put wstate :height height)
+  (.put wstate :num-agents num-agents)
+  (.put wstate :buildings buildings)
+  (.put wstate :roads roads)
+  (.put wstate :walkways walkways)
+  (.put wstate :agents agents)))
+	(start [this world] (let [wst (into {} (.wstate (.iworld world)))
+       agents (:agents wst)
+       buildings (:buildings wst)]
+  (.clear agents)
+  (.setMBR agents (.getMBR buildings))
+  (.scheduleRepeating (.schedule world) (.scheduleSpatialIndexUpdater agents) Integer/MAX_VALUE 1.0)))
 )
 (deftype IPortsImpl []
 	ru.igis.sim.IPorts
-	(createDisplay [this wgui] (def dispay (Display2D. WIDTH HEIGHT wgui)))
-	(setup [this world display] nil)
+	(createDisplay [this wgui world] (let [build-port (GeomVectorFieldPortrayal.)
+       road-port (GeomVectorFieldPortrayal.)
+       walk-port (GeomVectorFieldPortrayal.)
+       wstate (.wstate (.iworld world))
+       wst (into {} wstate)
+       display (Display2D. (:width wst) (:height wst) wgui)]
+  (.attach display build-port "Buildings" true)
+  (.attach display road-port "Roads" true)
+  (.attach display walk-port "Walkways" true)
+  (.put wstate :build-port build-port)
+  (.put wstate :road-port road-port)
+  (.put wstate :walk-port walk-port)
+  (println :DISP display)
+  display))
+	(setup [this display world] (let [wst (into {} (.wstate (.iworld world)))
+       build-port (:build-port wst)
+       road-port (:road-port wst)
+       walk-port (:walk-port wst)]
+  (.setField walk-port (:walkways wst))
+  (.setPortrayalForAll walk-port (GeomPortrayal. Color/PINK true))
+  (.setField build-port (:buildings wst))
+  (.setPortrayalForAll build-port (GeomPortrayal. Color/GRAY))
+  (.setField road-port (:roads wst))
+  (.setPortrayalForAll road-port (GeomPortrayal. Color/LIGHT_GRAY true))))
 )
