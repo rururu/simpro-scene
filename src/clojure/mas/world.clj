15690186fb4d2cb5017dbd60c32f31c652a4bcd6
rururu <zspovenetsky@gmail.com>
2019-08-11 22:41:04 +0300

    MASCampus works!

--- src/clojure/mas/world.clj
+++ src/clojure/mas/world.clj
@@ -11,13 +11,14 @@
   sim.display.Display2D
   sim.portrayal.geo.GeomPortrayal
   sim.portrayal.geo.GeomVectorFieldPortrayal
+  sim.portrayal.simple.OvalPortrayal2D
   com.vividsolutions.jts.geom.Envelope
   com.vividsolutions.jts.geom.Coordinate
   com.vividsolutions.jts.geom.GeometryFactory
   com.vividsolutions.jts.linearref.LengthIndexedLine))
 (def WIDTH 1000)
 (def HEIGHT 1000)
-(def NUM-AGENTS 10)
+(def NUM-AGENTS 1000)
 (def buildings (GeomVectorField. WIDTH HEIGHT))
 (def roads (GeomVectorField. WIDTH HEIGHT))
 (def walkways (GeomVectorField. WIDTH HEIGHT))
@@ -28,6 +29,7 @@
 (def build-port (GeomVectorFieldPortrayal.))
 (def road-port (GeomVectorFieldPortrayal.))
 (def walk-port (GeomVectorFieldPortrayal.))
+(def agent-port (GeomVectorFieldPortrayal.))
 (def declare-before (declare
   init-world
   start-world
@@ -38,13 +40,13 @@
 	sim.engine.Steppable
 	(step [this world] (move astate world))
 )
-(deftype IWorldImpl []
+(deftype CampusWorld []
 	ru.igis.sim.IWorld
 	(initialise [this] (load "mas/world")
 (init-world))
 	(start [this world] (start-world world))
 )
-(deftype IPortsImpl []
+(deftype CampusPorts []
 	ru.igis.sim.IPorts
 	(createDisplay [this wgui world] (create-display wgui world))
 	(setup [this display world] (setup-ports display world))
@@ -52,18 +54,18 @@
 (defn init-world []
   (let [netiter (.nodeIterator network)
        masked (Bag.)
-       _ (.add masked "NAME")
-       _ (.add masked "FLOORS")
-       _ (.add masked "ADDR_NUM")
-       _ (println "reading buildings layer") 
-       _ (ShapeFileImporter/read (URL. "file:data/campus/bldg.shp") buildings)
-       MBR (.getMBR buildings)
-       _ (println "reading roads layer") 
-       _ (ShapeFileImporter/read (URL. "file:data/campus/roads.shp") roads)
-       _ (.expandToInclude MBR (.getMBR roads))
-       _ (println "reading walkways layer")
-       _ (ShapeFileImporter/read (URL. "file:data/campus/walk_ways.shp") walkways)
-       _ (.expandToInclude MBR (.getMBR walkways))]
+       MBR (.getMBR buildings)]
+  (.add masked "NAME")
+  (.add masked "FLOORS")
+  (.add masked "ADDR_NUM")
+  (println "reading buildings layer") 
+  (ShapeFileImporter/read (URL. "file:data/campus/bldg.shp") buildings)
+  (println "reading roads layer") 
+  (ShapeFileImporter/read (URL. "file:data/campus/roads.shp") roads)
+  (.expandToInclude MBR (.getMBR roads))
+  (println "reading walkways layer")
+  (ShapeFileImporter/read (URL. "file:data/campus/walk_ways.shp") walkways)
+  (.expandToInclude MBR (.getMBR walkways))
   (println "Done reading data")
   (.setMBR buildings MBR)
   (.setMBR roads MBR)
@@ -75,10 +77,28 @@
            point (.createPoint factory coord)]
       (.addGeometry junctions (MasonGeometry. point))))))
 
-(defn move-to [coord]
-  nil)
+(defn move-to [coord point-to loc-geo]
+  (.setCoordinate point-to coord)
+(.apply loc-geo point-to)
+(.geometryChanged loc-geo))
 
-(defn set-new-route [line start astate]
+(defn find-new-path [world loc-geo]
+  (if-let [curr-junc (.findNode network (.getCoordinate loc-geo))]
+  (let [des (.getOutEdges curr-junc)
+         edges (.getEdges des)
+         nxt (.nextInt (.random world) (count edges))
+         dired (nth (seq edges) nxt)
+         edge (.getEdge dired)
+         newp (.getLine edge)
+         startp (.getStartPoint newp)
+         endp (.getEndPoint newp)]
+    (if (= startp loc-geo)
+      [newp true]
+      (if (= endp loc-geo)
+        [newp false]
+        (do (println "Where am I?") nil))))))
+
+(defn set-new-route [line start astate point-to loc-geo]
   (let [seg (LengthIndexedLine. line)
        sind (.getStartIndex seg)
        eind (.getEndIndex seg)]
@@ -90,15 +110,17 @@
     (do (vswap! astate assoc
             :curr-index sind
             :move-rate (:base-rate @astate))
-      (move-to (.extractPoint seg sind)))
+      (move-to (.extractPoint seg sind) point-to loc-geo))
     (do (vswap! astate assoc
             :curr-index eind
-            :move-rate -(:base-rate @astate))
-      (move-to (.extractPoint seg eind))))))
+            :move-rate (- (:base-rate @astate)))
+      (move-to (.extractPoint seg eind) point-to loc-geo)))))
 
 (defn create-astate [world]
   (let [point (.createPoint factory (Coordinate. 10 10))
        loc (MasonGeometry. point)
+       loc-geo (.getGeometry loc)
+       point-to (PointMoveTo.)
        base-rate 1.0
        astate (volatile! {:base-rate base-rate
                                  :move-rate base-rate
@@ -106,15 +128,14 @@
                                  :start-index 0.0
                                  :end-index 0.0
                                  :curr-index 0.0
-                                 :point-to (PointMoveTo.)
+                                 :point-to point-to
                                  })
        rand (.random world)
        ww-geos (.getGeometries walkways)
        wwn (.nextInt rand (.numObjs ww-geos))
        mg (.get ww-geos wwn)]
   (set! (.isMovable loc) true)
-  (set-new-route (.getGeometry mg) true astate)
-  (println :LOC loc)
+  (set-new-route (.getGeometry mg) true astate point-to loc-geo)
   (if (.nextBoolean rand)
          (do (.addStringAttribute loc "TYPE" "STUDENT")
                (.addIntegerAttribute loc "AGE"
@@ -148,10 +169,33 @@
   (.attach display build-port "Buildings" true)
   (.attach display road-port "Roads" true)
   (.attach display walk-port "Walkways" true)
+  (.attach display agent-port "Agents" true)
   display))
 
 (defn move [astate world]
-  (println :AS astate))
+  (let [ast @astate
+       move-rate (:move-rate ast)
+       curr-index (:curr-index ast)
+       start-index (:start-index ast)
+       end-index (:end-index ast)
+       point-to (:point-to ast)
+       location (:location ast)
+       loc-geo (.getGeometry location)]
+  (if (or (and (> move-rate 0) (>= curr-index end-index))
+           (and (< move-rate 0) (<= curr-index start-index)))
+    (let [[newp start] (find-new-path world loc-geo)]
+      (set-new-route newp start astate point-to loc-geo))
+    (let [curr-index (+ curr-index move-rate)
+           curr-index (if (< move-rate 0)
+                              (if (< curr-index start-index)
+                                  start-index
+                                  curr-index)
+                              (if (> curr-index end-index)
+                                  end-index
+                                  curr-index))
+           curr-pos (.extractPoint (:segment ast) curr-index)]
+      (move-to curr-pos point-to loc-geo)
+      (vswap! astate assoc :curr-index curr-index)))))
 
 (defn setup-ports [display world]
   (.setField walk-port walkways)
@@ -159,5 +203,19 @@
 (.setField build-port buildings)
 (.setPortrayalForAll build-port (GeomPortrayal. Color/GRAY))
 (.setField road-port roads)
-(.setPortrayalForAll road-port (GeomPortrayal. Color/LIGHT_GRAY true)))
+(.setPortrayalForAll road-port (GeomPortrayal. Color/LIGHT_GRAY true))
+(.setField agent-port agents)
+(.setPortrayalForAll agent-port (OvalPortrayal2D. Color/RED 10.0)))
+
+(defn run-world-gui [hm inst]
+  (let [mp (into {} hm)
+       iwd (mp "iWorld-class")
+       ipts (mp "iPorts-class")
+       cwd (Class/forName iwd)
+       cpts (Class/forName ipts)
+       cwd (.getConstructors cwd)
+       cpts (.getConstructors cpts)
+       iwd (.newInstance (first cwd) nil)
+       ipts (.newInstance (first cpts) nil)]
+  (ru.igis.sim.WorldGUI/run iwd ipts)))
 
