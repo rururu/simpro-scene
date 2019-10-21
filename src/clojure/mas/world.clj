02713826f7b7da4ba6824b93855c364ab6d76f07
rururu <zspovenetsky@gmail.com>
2019-10-21 23:06:10 +0300

    LineFollower by attribute value and One Run for debugging

--- src/clojure/mas/world.clj
+++ src/clojure/mas/world.clj
@@ -1,11 +1,18 @@
 (ns mas.world
+(:use protege.core)
 (:require
   clojure.java.shell))
-(defn run-world-gui [hm inst]
+(defn mult-run-world-gui [hm inst]
   (let [mp (into {} hm)
        iwd (mp "iWorld-class")
        ipts (mp "iPorts-class")
        cmd (str "(ru.igis.sim.WorldGUI/main (into-array String [\"" iwd "\" \"" ipts "\"]))")]
   (println cmd)
   (println (clojure.java.shell/sh  "clj" "-e" cmd))))
+
+(defn one-run-world-gui [hm inst]
+  (let [mp (into {} hm)
+       iwd (mp "iWorld-class")
+       ipts (mp "iPorts-class")]
+  (invoke-later (ru.igis.sim.WorldGUI/main (into-array String [(str iwd) (str ipts)])))))
 
