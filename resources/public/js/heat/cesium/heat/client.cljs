802c3f169b3dd8a6867d899d69fa1c62dcb662b7
rururu <zspovenetsky@gmail.com>
2020-05-13 22:42:28 +0300

    Cesium Heatmap project

--- resources/public/js/heat/cesium/heat/client.cljs
+++ resources/public/js/heat/cesium/heat/client.cljs
@@ -2,31 +2,52 @@
 (:require 
   [cesium.client :refer
     [add-terrain
+     camera-home
      start-event-processing
      add-orbit-button
      VIEWER
      CAMERA]]))
+(def MY-HOME-VIEW {:latitude 60.0
+  :longitude 30.15
+  :height 2000
+  :heading 130
+  :pitch -15
+  :roll 0})
+(def HEATMAP nil)
+(def DATA [[30.278 59.971 5]
+ [30.395 59.956 7]
+ [30.252 59.917 8]
+ [30.343 59.919 6]])
+(def BOUNDS {:west 30.252
+  :east 30.395
+  :south 59.917
+  :north 59.971})
+(def OPTIONS {:radius 600
+  :maxOpacity 0.8
+  :scaleRadius false
+  :blur 0.85
+  :useLocalExtrema false})
+(defn heatmap-init [bounds options]
+  (def HEATMAP
+  (js/CesiumHeatmap.create 
+    VIEWER 
+    (clj->js bounds) 
+    (clj->js options))))
+
+(defn heatmap-data [valmin valmax data]
+  (.setWGS84Data HEATMAP
+  valmin 
+  valmax 
+  (clj->js (for [[x y v] data] (hash-map "x" x "y" y "value" v)))))
+
 (defn init-heat []
   (add-terrain nil VIEWER)
+(camera-home CAMERA MY-HOME-VIEW)
 (start-event-processing VIEWER)
 (add-orbit-button CAMERA))
-
-(defn tst []
-  (let [bnds #js{:west 147.13833844
-                       :east 147.13856899
-                       :south -41.43606916
-                       :north -41.43582929}
-       data [{"x" 147.1383442264 "y" -41.4360048372 "value" 76}
-                 {"x" 147.1384363011 "y" -41.4360298848 "value" 63}
-                  {"x" 147.138368102 "y" -41.4358360603 "value" 1}
-                  {"x" 147.1385627739 "y" -41.4358799123 "value" 21}]
-       hm (js/CesiumHeatmap.create VIEWER bnds {})
-       valmin 0
-       valmax 100]
-  (.setWGS84Data hm valmin valmax (clj->js data))
-  (println :HM data)))
 
 
 (enable-console-print!)
 (set! (.-onload js/window) (init-heat))
-(tst)
+(heatmap-init BOUNDS OPTIONS)
+(heatmap-data 0 8 DATA)
--- resources/public/js/heat/cesium/heat/client.js
+++ resources/public/js/heat/cesium/heat/client.js
@@ -2,25 +2,79 @@
 goog.provide('cesium.heat.client');
 goog.require('cljs.core');
 goog.require('cesium.client');
+cesium.heat.client.MY_HOME_VIEW = new cljs.core.PersistentArrayMap(null, 6, [new cljs.core.Keyword(null,"latitude","latitude",394867543),60.0,new cljs.core.Keyword(null,"longitude","longitude",-1268876372),30.15,new cljs.core.Keyword(null,"height","height",1025178622),(2000),new cljs.core.Keyword(null,"heading","heading",-1312171873),(130),new cljs.core.Keyword(null,"pitch","pitch",1495126700),(-15),new cljs.core.Keyword(null,"roll","roll",11266999),(0)], null);
+cesium.heat.client.HEATMAP = null;
+cesium.heat.client.DATA = new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [30.278,59.971,(5)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [30.395,59.956,(7)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [30.252,59.917,(8)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [30.343,59.919,(6)], null)], null);
+cesium.heat.client.BOUNDS = new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"west","west",708776677),30.252,new cljs.core.Keyword(null,"east","east",1189821678),30.395,new cljs.core.Keyword(null,"south","south",1586796293),59.917,new cljs.core.Keyword(null,"north","north",651323902),59.971], null);
+cesium.heat.client.OPTIONS = new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"radius","radius",-2073122258),(600),new cljs.core.Keyword(null,"maxOpacity","maxOpacity",-33085019),0.8,new cljs.core.Keyword(null,"scaleRadius","scaleRadius",-59738660),false,new cljs.core.Keyword(null,"blur","blur",-453500461),0.85,new cljs.core.Keyword(null,"useLocalExtrema","useLocalExtrema",1349969296),false], null);
+cesium.heat.client.heatmap_init = (function cesium$heat$client$heatmap_init(bounds,options){
+return (
+cesium.heat.client.HEATMAP = CesiumHeatmap.create(cesium.client.VIEWER,cljs.core.clj__GT_js.call(null,bounds),cljs.core.clj__GT_js.call(null,options)))
+;
+});
+cesium.heat.client.heatmap_data = (function cesium$heat$client$heatmap_data(valmin,valmax,data){
+return cesium.heat.client.HEATMAP.setWGS84Data(valmin,valmax,cljs.core.clj__GT_js.call(null,(function (){var iter__4434__auto__ = (function cesium$heat$client$heatmap_data_$_iter__14479(s__14480){
+return (new cljs.core.LazySeq(null,(function (){
+var s__14480__$1 = s__14480;
+while(true){
+var temp__5735__auto__ = cljs.core.seq.call(null,s__14480__$1);
+if(temp__5735__auto__){
+var s__14480__$2 = temp__5735__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__14480__$2)){
+var c__4432__auto__ = cljs.core.chunk_first.call(null,s__14480__$2);
+var size__4433__auto__ = cljs.core.count.call(null,c__4432__auto__);
+var b__14482 = cljs.core.chunk_buffer.call(null,size__4433__auto__);
+if((function (){var i__14481 = (0);
+while(true){
+if((i__14481 < size__4433__auto__)){
+var vec__14483 = cljs.core._nth.call(null,c__4432__auto__,i__14481);
+var x = cljs.core.nth.call(null,vec__14483,(0),null);
+var y = cljs.core.nth.call(null,vec__14483,(1),null);
+var v = cljs.core.nth.call(null,vec__14483,(2),null);
+cljs.core.chunk_append.call(null,b__14482,cljs.core.PersistentHashMap.fromArrays(["x","y","value"],[x,y,v]));
+
+var G__14489 = (i__14481 + (1));
+i__14481 = G__14489;
+continue;
+} else {
+return true;
+}
+break;
+}
+})()){
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__14482),cesium$heat$client$heatmap_data_$_iter__14479.call(null,cljs.core.chunk_rest.call(null,s__14480__$2)));
+} else {
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__14482),null);
+}
+} else {
+var vec__14486 = cljs.core.first.call(null,s__14480__$2);
+var x = cljs.core.nth.call(null,vec__14486,(0),null);
+var y = cljs.core.nth.call(null,vec__14486,(1),null);
+var v = cljs.core.nth.call(null,vec__14486,(2),null);
+return cljs.core.cons.call(null,cljs.core.PersistentHashMap.fromArrays(["x","y","value"],[x,y,v]),cesium$heat$client$heatmap_data_$_iter__14479.call(null,cljs.core.rest.call(null,s__14480__$2)));
+}
+} else {
+return null;
+}
+break;
+}
+}),null,null));
+});
+return iter__4434__auto__.call(null,data);
+})()));
+});
 cesium.heat.client.init_heat = (function cesium$heat$client$init_heat(){
 cesium.client.add_terrain.call(null,null,cesium.client.VIEWER);
+
+cesium.client.camera_home.call(null,cesium.client.CAMERA,cesium.heat.client.MY_HOME_VIEW);
 
 cesium.client.start_event_processing.call(null,cesium.client.VIEWER);
 
 return cesium.client.add_orbit_button.call(null,cesium.client.CAMERA);
 });
-cesium.heat.client.tst = (function cesium$heat$client$tst(){
-var bnds = ({"west": 147.13833844, "east": 147.13856899, "south": -41.43606916, "north": -41.43582929});
-var data = new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentArrayMap(null, 3, ["x",147.1383442264,"y",-41.4360048372,"value",(76)], null),new cljs.core.PersistentArrayMap(null, 3, ["x",147.1384363011,"y",-41.4360298848,"value",(63)], null),new cljs.core.PersistentArrayMap(null, 3, ["x",147.138368102,"y",-41.4358360603,"value",(1)], null),new cljs.core.PersistentArrayMap(null, 3, ["x",147.1385627739,"y",-41.4358799123,"value",(21)], null)], null);
-var hm = CesiumHeatmap.create(cesium.client.VIEWER,bnds,cljs.core.PersistentArrayMap.EMPTY);
-var valmin = (0);
-var valmax = (100);
-hm.setWGS84Data(valmin,valmax,cljs.core.clj__GT_js.call(null,data));
-
-return cljs.core.println.call(null,new cljs.core.Keyword(null,"HM","HM",-1913844374),data);
-});
 cljs.core.enable_console_print_BANG_.call(null);
 window.onload = cesium.heat.client.init_heat.call(null);
-cesium.heat.client.tst.call(null);
+cesium.heat.client.heatmap_init.call(null,cesium.heat.client.BOUNDS,cesium.heat.client.OPTIONS);
+cesium.heat.client.heatmap_data.call(null,(0),(8),cesium.heat.client.DATA);
 
 //# sourceMappingURL=client.js.map
