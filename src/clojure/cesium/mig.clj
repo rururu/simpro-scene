a8d3789b730f487af6b086248997362c5486f562
rururu <zspovenetsky@gmail.com>
2020-06-18 20:07:57 +0300

    Lakes can be handled separately

--- src/clojure/cesium/mig.clj
+++ src/clojure/cesium/mig.clj
@@ -40,6 +40,9 @@
   (let [pts (map #(str (MapOb/getDegMin (second %)) " " (MapOb/getDegMin (first %))) pts)]
   (ssvs inst "points" pts)))
 
+(defn simple-dist [[lo1 la1 & _] [lo2 la2 & _]]
+  (+ (Math/abs (- lo1 lo2)) (Math/abs (- la1 la2))))
+
 (defn gv-field-from-shape
   ([shp]
   (let [dbf (str (.substring shp 0 (- (count shp) 4)) ".dbf")]
@@ -49,23 +52,53 @@
     (ShapeFileImporter/read (URL. shp) (URL. dbf) gvf)
     gvf)))
 
-(defn simple-dist [[lo1 la1 & _] [lo2 la2 & _]]
-  (+ (Math/abs (- lo1 lo2)) (Math/abs (- la1 la2))))
+(defn geoms-by-attrs [attrs vals gv-field]
+  (map #(.geometry (.getGeometry gv-field %1 (AttributeValue. %2))) attrs vals))
 
-(defn from-gv-field-by-attributes [gv-field col-vals start height]
-  (loop [[[col val] & cvrst] col-vals path [(conj start height)]]
-  (if (some? col)
-    (let [ps (map #(list (.x %) (.y %) height) (.getCoordinates (.geometry (.getGeometry gv-field col (AttributeValue. val)))))
-           p1 (last path)
-           p2 (first ps)
-           p3 (last ps)
-           d1 (simple-dist p1 p2)
-           d2 (simple-dist p1 p3)
-           nxt (if (< d2 d1)
-                   (reverse ps)
-                   ps)] 
-      (recur cvrst (concat path nxt)))
-    path)))
+(defn geoms-to-path [geoms start height]
+  (let [lss (map #(.getCoordinates %) geoms)
+       lss (map #(list (.x %) (.y %) height) lss)]
+  (loop [[ps & rss] lss path [(conj start height)]]
+    (if (some? ps)
+      (let [p1 (last path)
+             p2 (first ps)
+             p3 (last ps)
+             d1 (simple-dist p1 p2)
+             d2 (simple-dist p1 p3)
+             nxt (if (< d2 d1)
+                     (reverse ps)
+                     ps)] 
+        (recur rss (concat path nxt)))
+      path))))
+
+(defn river-map [name]
+  (or (@RIVERS name)
+  (let [rinst (fifos "River" "title" name)
+         lay (sv rinst "layer")
+         ats (svs rinst "attrs")
+         vls (svs rinst "values")
+         gvf (gv-field-from-shape (sv lay "shapeFile"))
+         mp {:layer (sv lay "prettyName")
+                 :gv-field gvf
+                 :geoms (geoms-by-attrs ats vls)
+                 :head (read-string (sv rinst "head"))
+                 :estuary (read-string (sv rinst "estuary"))
+                 :flow-speed (sv rinst "flow-speed")}]
+    (vswap! RIVERS assoc name mp)
+    mp)))
+
+(defn lake-map [name]
+  (or (@LAKES name)
+  (let [linst (fifos "Lake" "title" name)
+         lay (sv linst "layer")
+         ats (svs linst "attrs")
+         vls (svs linst "values")
+         gvf (gv-field-from-shape (sv lay "shapeFile"))
+         mp {:layer (sv lay "prettyName")
+                 :gv-field gvf
+                 :geoms (geoms-by-attrs ats vls gvf)}]
+    (vswap! LAKES assoc name mp)
+    mp)))
 
 (defn rand-step [s]
   (- (* 2 s (Math/random)) s))
@@ -78,72 +111,76 @@
     (< start finish) (+ start s)
     true (- start s))))
 
+(defn covered-by [lon lat geoms]
+  (let [pnt (.createPoint GeomFACTORY (Coordinate. lon lat))]
+  (some #(.coveredBy pnt %) geoms)))
+
 (defn next-covered
-  ([lon lat slon step gv-field]
+  ([lon lat slon step geoms]
   (loop [i 4 sl slon st step]
     (if (> i 0)
-      (if-let [ncvd (next-covered lon lat sl st 10 gv-field)]
+      (if-let [ncvd (next-covered lon lat sl st 10 geoms)]
         ncvd
         (recur (dec i) (* sl 2) (* st 2)))
       [lon lat])))
-([lon lat slon step n gv-field]
+([lon lat slon step n geoms]
   (loop [j n]
     (if (> j 0)
       (let [lon1 (+ lon (rand-step slon))
              lat1 (+ lat (rand-step step))]
-        (if (.isCovered gv-field (Coordinate. lon1 lat1))
+        (if (covered-by lon1 lat1 geoms)
           [lon1 lat1]
           (recur (dec j))))))))
 
-(defn next-covered-closer [lo1 la1 lo2 la2 slon step gv-field]
+(defn next-covered-closer [lo1 la1 lo2 la2 slon step geoms]
   (let [lo3 (rand-step-closer slon lo1 lo2)
        la3 (rand-step-closer step la1 la2)]
-  (if (.isCovered gv-field (Coordinate. lo3 la3))
+  (if (covered-by lo3 la3 geoms)
     [lo3 la3]
-    (next-covered lo1 la1 slon step gv-field))))
+    (next-covered lo1 la1 slon step geoms))))
 
-(defn random-walk [start steps step height gv-field]
+(defn random-walk [start steps step height geoms]
   (let [phi (Math/toRadians (second start))
        slon (/ step (Math/cos phi))]
   (loop [n steps [lon lat] start path [(conj start height)]]
     (if (> n 0)
-      (let [nxt (next-covered lon lat slon step gv-field)]
+      (let [nxt (next-covered lon lat slon step geoms)]
         (recur (dec n) nxt (conj path (conj nxt height))))
       path))))
 
-(defn random-walk-closer [start finish steps step height gv-field]
+(defn random-walk-closer [start finish steps step height geoms]
   (let [phi (Math/toRadians (second start))
        slon (/ step (Math/cos phi))
        [lof laf] finish]
   (loop [n steps [los las] start path [(conj start height)]]
     (if (> n 0)
-      (let [[lor lar] (next-covered los las slon step gv-field)
-             [lon lan :as nxt] (next-covered-closer lor lar lof laf slon step gv-field)
+      (let [[lor lar] (next-covered los las slon step geoms)
+             [lon lan :as nxt] (next-covered-closer lor lar lof laf slon step geoms)
              newp (conj path (conj nxt height))]
         (if (and (== lon lof) (== lan laf))
           newp
           (recur (dec n) nxt newp)))
       path))))
 
-(defn random-by-waypoints [wps limstp steps step height gv-field]
+(defn random-by-waypoints [wps limstp steps step height geoms]
   (loop [[s f & r :as ws] wps path []]
   (cond
     (> (count path) limstp)
       path
     (and s f)
-      (let [rwc1 (random-walk-closer s f steps step height gv-field)]
+      (let [rwc1 (random-walk-closer s f steps step height geoms)]
         (if (< (count rwc1) steps)
           (recur (rest ws) (concat path rwc1))
-          (let [rwc2 (random-walk-closer f s steps step height gv-field)]
+          (let [rwc2 (random-walk-closer f s steps step height geoms)]
             (if (< (count rwc2) steps)
               (recur (rest ws) (concat path (reverse rwc2)))
               (recur (cons (last rwc1) (rest ws)) (concat path rwc1))))))
     true
       path)))
 
-(defn go-gv-field-attributes [id color size knots height start gv-field attrs]
+(defn go-geoms-path [id color size knots height start geoms]
   ;; returns time of going in sec
-(let [pts (from-gv-field-by-attributes gv-field attrs start height)
+(let [pts (geoms-to-path geoms start height)
        func-dist #(com.bbn.openmap.proj.GreatCircle/sphericalDistance %1 %2 %3 %4)
        mils (+ (Clock/getClock) 2000)
        [czml elt] (cg/add-point-flight id pts knots mils "RELATIVE_TO_GROUND" color size func-dist)]
@@ -166,7 +203,7 @@
 (let [[lah loh] (river :head)
         [lae loe] (river :estuary)
         gvf (river :gv-field)]
-  (go-gv-field-attributes id color size
+  (go-geoms-path id color size
     (condp = direction
       :down (+ knots (river :flow-speed))
       :up knots)
@@ -174,14 +211,13 @@
     (condp = direction
       :down (river :head)
       :up (river :estuary))
-    (river :gv-field)
     (condp = direction
-      :down (river :path)
-      :up (reverse (river :path))))))
+      :down (river :geoms)
+      :up (reverse (river :geoms))))))
 
-(defn go-random-by-waypoints [id color size knots height wps limstp steps step gv-field]
+(defn go-random-by-waypoints [id color size knots height wps limstp steps step geoms]
   ;; returns time of going in sec
-(let [wps (random-by-waypoints wps limstp steps step height gv-field)
+(let [wps (random-by-waypoints wps limstp steps step height geoms)
        func-dist #(com.bbn.openmap.proj.GreatCircle/sphericalDistance %1 %2 %3 %4)
        mils (+ (Clock/getClock) 2000)
        [czml elt] (cg/add-point-flight id wps knots mils "RELATIVE_TO_GROUND" color size func-dist)]
@@ -201,32 +237,6 @@
               :step "SYSTEM_CLOCK_MULTIPLIER"
               :range "UNBOUNDED"}]
   (cs/send-clock cs)))
-
-(defn river-map [name]
-  (or (@RIVERS name)
-  (let [rinst (fifos "River" "title" name)
-         lay (sv rinst "layer")
-         mp {:layer (sv lay "prettyName")
-                 :head (read-string (sv rinst "head"))
-                 :path (vec (map #(vector %1 %2) (svs rinst "attrs") (svs rinst "values")))
-                 :estuary (read-string (sv rinst "estuary"))
-                 :flow-speed (sv rinst "flow-speed")
-                 :gv-field (gv-field-from-shape (sv lay "shapeFile"))}]
-    (vswap! RIVERS assoc name mp)
-    mp)))
-
-(defn lake-map [name]
-  (or (@LAKES name)
-  (let [linst (fifos "Lake" "title" name)
-         lay (sv linst "layer")
-         atr (sv linst "attrs")
-         val (sv linst "values")
-         gvf (gv-field-from-shape (sv lay "shapeFile"))
-         mp {:layer (sv lay "prettyName")
-                 :gv-field gvf
-                 :polygon (.geometry (.getGeometry gvf atr (AttributeValue. val)))}]
-    (vswap! LAKES assoc name mp)
-    mp)))
 
 (defn clock-scale []
   [(Clock/getClock) (Clock/getTimeScale)])
