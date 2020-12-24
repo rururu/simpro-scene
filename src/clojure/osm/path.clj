e78316437c92fa94fe09c119eea744438ecfdc3b
rururu <zspovenetsky@gmail.com>
2020-12-24 22:41:59 +0300

    OSMTrailRun not working!

--- src/clojure/osm/path.clj
+++ src/clojure/osm/path.clj
@@ -9,6 +9,8 @@
 (:import
   ru.igis.omtab.OMT
   ru.igis.omtab.MapOb))
+(def TRACE true)
+(def NEAR 0.0005)
 (defn simple-dist-osm
   ([ni1 ni2]
   (of/simple-dist 
@@ -45,4 +47,39 @@ nis)
        dirs (map simple-dir-osm sgs)
        rdir (simple-dir-osm [start finish])]
   (some #(is-backward-dir? % rdir) dirs)))
+
+(defn if-display-edges [pfx egs]
+  (if TRACE 
+  (println pfx (map #(sv % "label") egs)))
+egs)
+
+(defn vertex1 [e]
+  [(sv e "x") (sv e "y")])
+
+(defn vertex2 [e]
+  [(sv e "x2") (sv e "y2")])
+
+(defn far-vertex [e pnt]
+  (let [v1 (vertex1 e)
+       v2 (vertex2 e)]
+  (if (> (of/simple-dist v1 pnt) (of/simple-dist v2 pnt))
+    v1
+    v2)))
+
+(defn edges-distance [egs]
+  (apply + (map #(of/simple-dist (vertex1 %) (vertex2 %)) egs)))
+
+(defn wrong-direction [start finish egs]
+  (let [dirs (map #(of/simple-dir (vertex1 %) (vertex2 %)) egs)
+      rdir (of/simple-dir start finish)]
+  (some #(not= % rdir) dirs)))
+
+(defn near [p1 p2]
+  (< (of/simple-dist p1 p2) NEAR))
+
+(defn set-near [deg]
+  (def NEAR deg))
+
+(defn trace [bool]
+  (def TRACE bool))
 
